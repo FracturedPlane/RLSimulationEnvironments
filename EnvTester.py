90d4b66b3432942f6f9ef377159aea3180e941c6
Neo-X <glen@fracturedplane.com>
2018-07-08 10:52:20 -0400

    Working on viz testing

--- EnvTester.py
+++ EnvTester.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     
     # env = getEnv(env_name="ParticleGame_2D-v0", render=False)
     # env = getEnv(env_name="CannonGameViz2-v0", render=True)
-    env = getEnv(env_name="ProjectileGameViz-DualState-v0", render=True)
+    env = getEnv(env_name="ProjectileGameViz-DualState-v0", render=False)
 
     actionSpace = env.getActionSpace()
     env.setRandomSeed(1234)
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         # while (True):
         for i in range(100):
             actions = []
-            for i in range(env.getNumberofAgents()):
+            for a in range(env.getNumberofAgents()):
                 action = ((actionSpace.getMaximum() - actionSpace.getMinimum()) * np.random.uniform(size=actionSpace.getMinimum().shape[0])  ) + actionSpace.getMinimum()
                 actions.append(action)
             if (env.getNumberofAgents() > 1):
@@ -42,20 +42,27 @@ if __name__ == '__main__':
                 viewImitateData = vizImitateData[vd]
                 ## Get and vis terrain data
                 if (True):
+                    ## Don't use Xwindows backend for this
+                    import matplotlib
+                    matplotlib.use('Agg')
                     import matplotlib.pyplot as plt
                     # img_ = np.reshape(viewData, (150,158,3))
                     img_ = viewData
                     print("img_ shape", img_.shape, " sum: ", np.sum(viewData))
-                    plt.figure(1)
+                    fig1 = plt.figure(1)
                     plt.imshow(img_, origin='lower')
                     plt.title("visual Data: " +  str(vd))
+                    fig1.savefig("viz_state_"+str(i)+".svg")
 
                     if (True):                    
                         img_ = viewImitateData
-                        plt.figure(2)
+                        fig2 = plt.figure(2)
                         plt.imshow(img_, origin='lower')
                         plt.title("visual Data: " +  str(vd))
-                    plt.show()
+                        fig2.savefig("viz_imitation_state_"+str(i)+".svg")
+                        
+                    # plt.show()
+                    
             img = env.getEnv().getVisualState()
             if ( done ):
                 break
