0254d7be868469c52025c11c16b8d583a7b963ff
Neo-X <glen@fracturedplane.com>
2019-01-28 15:10:54 -0800

    Working on new environments

---
+++ rlsimenv/NavGame2D.py
@@ -0,0 +1,47 @@
+
+import pybullet as p
+import pybullet_data
+import os
+import time
+GRAVITY = -9.8
+dt = 1e-3
+iters=2000
+
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.resetSimulation()
+#p.setRealTimeSimulation(True)
+p.setGravity(0,0,GRAVITY)
+p.setTimeStep(dt)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,1.0]
+cubeStartOrientation = p.getQuaternionFromEuler([0.,0,0])
+botId = p.loadURDF("sphere2.urdf",
+        cubeStartPos,
+        cubeStartOrientation) 
+
+blockId = p.loadURDF("cube2.urdf",
+        [2.0,2.0,0.5],
+        cubeStartOrientation,
+        useFixedBase=1) 
+
+ 
+#disable the default velocity motors 
+#and set some position control with small force to emulate joint friction/return to a rest pose
+jointFrictionForce=1
+for joint in range (p.getNumJoints(botId)):
+    p.setJointMotorControl2(botId,joint,p.POSITION_CONTROL,force=jointFrictionForce)
+
+#for i in range(10000):     
+#     p.setJointMotorControl2(botId, 1, p.TORQUE_CONTROL, force=1098.0)
+#     p.stepSimulation()
+#import ipdb
+#ipdb.set_trace()
+import time
+p.setRealTimeSimulation(1)
+while (1):
+    p.stepSimulation()
+    # p.setJointMotorControl2(botId, 1, p.TORQUE_CONTROL, force=1098.0)
+    p.setGravity(0,0,GRAVITY)
+    time.sleep(1/240.)
+time.sleep(1000)
