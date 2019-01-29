ac6c3e4c1039f6ee85f02da3a7f681353f236f70
Neo-X <glen@fracturedplane.com>
2019-01-28 16:21:04 -0800

    Working on new environments

--- rlsimenv/NavGame2D.py
+++ rlsimenv/NavGame2D.py
@@ -9,10 +9,11 @@ class NavGame2D(object):
     
     def __init__(self):
         self._GRAVITY = -9.8
-        self._dt = 1e-3
+        self._dt = 1/50.0
         self._iters=2000 
 
     def init(self):
+        
         self._physicsClient = p.connect(p.GUI)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.resetSimulation()
@@ -20,7 +21,8 @@ class NavGame2D(object):
         p.setGravity(0,0,self._GRAVITY)
         p.setTimeStep(self._dt)
         planeId = p.loadURDF("plane.urdf")
-        cubeStartPos = [0,0,1.0]
+        
+        cubeStartPos = [0,0,0.5]
         cubeStartOrientation = p.getQuaternionFromEuler([0.,0,0])
         self._agent = p.loadURDF("sphere2.urdf",
                 cubeStartPos,
@@ -44,15 +46,26 @@ class NavGame2D(object):
         #import ipdb
         #ipdb.set_trace()
         
+    def reset(self):
+        import numpy as np
+        x = (np.random.rand()-0.5) * 10
+        y = (np.random.rand()-0.5) * 10
+        p.resetBasePositionAndOrientation(self._agent, [x,y,0.5], p.getQuaternionFromEuler([0.,0,0]))
+        
+        
     def run(self):
         import time
         p.setRealTimeSimulation(1)
-        while (1):
-            p.stepSimulation()
+        for i in range(10000):
+            if (i % 10 == 0):
+                self.reset()
+            # p.stepSimulation()
             # p.setJointMotorControl2(botId, 1, p.TORQUE_CONTROL, force=1098.0)
             p.setGravity(0,0,self._GRAVITY)
             time.sleep(1/240.)
+            # self.act([0.1,0.1])
             self.getObservation()
+            
         time.sleep(1000)
         
     def getObservation(self):
@@ -69,24 +82,37 @@ class NavGame2D(object):
     def getlocalMapObservation(self):
         ### raycast around the area of the agent
         import numpy as np
+        
+        pos = p.getBasePositionAndOrientation(self._agent)[0]
         ### number of samples
-        size = 8
+        size = 16
         ### width of box
-        dimensions = 2.0
+        dimensions = 8.0
         toRays = []
         for i in range(0,size):
             for j in range(0,size):
                 toRays.append([(1.0/(size * 1.0))*i*dimensions,(1.0/(size * 1.0))*j*dimensions,0])
-        print (len(toRays))
+        assert (len(toRays) == (size*size))
         toRays = np.array(toRays)
-        print ("toRays:", toRays )
+        ### Adjust to put agent in middle of map
+        toRays = toRays + pos - np.array([dimensions/2.0, dimensions/2.0, 0])
+        # print ("toRays:", toRays )
         
         fromRays = toRays + np.array([0,0,5])
         rayResults = p.rayTestBatch(fromRays, toRays)
         intersections = [ray[0] for ray in rayResults]
         # print (rayResults)
         intersections = np.array(np.greater(intersections, 0), dtype="int")
+        intersections = np.reshape(intersections, (size, size))
         print ("intersections", intersections)
+        return intersections
+    
+    def act(self, action):
+        import numpy as np
+        ### apply delta position change.
+        action = np.array([action[0], action[1], 0])
+        pos = p.getBasePositionAndOrientation(self._agent)[0]
+        p.resetBasePositionAndOrientation(self._agent, pos + action, p.getQuaternionFromEuler([0.,0,0]))
         
         
         
